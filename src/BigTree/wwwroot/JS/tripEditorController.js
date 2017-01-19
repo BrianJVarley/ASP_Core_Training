f450f7fd3aef350a1cc2e19da0b4377f7b23c90a
BrianJVarley <brianvarley9@gmail.com>
2017-01-19 21:39:41 +0000

    Validating stops with GMaps lib

--- src/BigTree/wwwroot/JS/tripEditorController.js
+++ src/BigTree/wwwroot/JS/tripEditorController.js
@@ -7,7 +7,7 @@
 	angular.module("app-trips")
 	.controller("tripEditorController", tripEditorController);
 
-	function tripEditorController($routeParams) {
+	function tripEditorController($routeParams, $http) {
 
 	    var vm = this;
 
@@ -15,6 +15,70 @@
 	    vm.stops = [];
 	    vm.errorMessage = "";
 	    vm.isBusy = true;
+	    vm.newStop = {};
+
+	    var url = "/api/trips/" + vm.tripName + "/stops";
+
+	    $http.get(url)
+            .then(function (response) {
+                //success
+                angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
+            }, function (err) {
+                //failure
+                vm.errorMessage = "Failed to load stops";
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+	    vm.addStop = function () {
+
+	        vm.isBusy = true;
+
+	        $http.post(url, vm.newStop)
+                .then(function (response) {
+                    //success
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function (err) {
+                    //failure
+                    vm.errorMessage = "Failed to add new stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+	    }
+
+	    function _showMap(stops) {
+
+	        
+	        if(stops && stops.length > 0)
+	        {
+
+	            var mapStops = _.map(stops, function (item) { //convert the stops to match api spec on lon,lat.
+	                return {
+	                    lat: item.latitude,
+	                    long: item.longitude,
+	                    info: item.name
+	                };
+
+	            });
+
+
+
+	            //show map
+	            travelMap.createMap({
+	                stops: mapStops,
+	                selector: "#map",
+	                currentStop: 1,
+                    initialZoom: 3
+
+	            })
+	        }
+
+	    }
 
 	}
 
